6957d71485996f4c9d4ab059632d902f2db15eba
offdutysysadmin <jonmoo@offdutysysadmin.com>
2016-01-05 12:14:06 -0600

    updates

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,35 +1,89 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*- #
-from __future__ import unicode_literals
+# Theme-specific settings
+SITENAME = "Captain's Log"
+DOMAIN = 'captainslog.me'
+BIO_TEXT = ''
+FOOTER_TEXT = ''
 
-AUTHOR = u'Jon Moore'
-SITENAME = u"Captain's Log"
-SITEURL = ''
+SITE_AUTHOR = 'Jon Moore'
+TWITTER_USERNAME = '@offdutypirate'
+GOOGLE_PLUS_URL = ''
+INDEX_DESCRIPTION = ''
 
-PATH = 'content'
+SIDEBAR_LINKS = [
+    '<a href="/about/">About</a>',
+    '<a href="/archive/">Archive</a>',
+]
 
+ICONS_PATH = 'images/icons'
+
+GOOGLE_FONTS = [
+    "Raleway:400,600",
+    "Source Code Pro",
+]
+
+SOCIAL_ICONS = [
+    ('mailto:mail@captainslog.me', 'Email', 'fa-envelope'),
+    ('http://twitter.com/offdutypirate', 'Twitter', 'fa-twitter'),
+    ('http://github.com/offdutypirate', 'GitHub', 'fa-github'),
+    ('/atom.xml', 'Atom Feed', 'fa-rss'),
+]
+
+THEME_COLOR = '#FF8000'
+
+# Pelican settings
+RELATIVE_URLS = True
+SITEURL = 'http://localhost'
 TIMEZONE = 'America/Chicago'
+DEFAULT_DATE = 'fs'
+DEFAULT_DATE_FORMAT = '%B %d, %Y'
+DEFAULT_PAGINATION = False
+SUMMARY_MAX_LENGTH = 42
 
-DEFAULT_LANG = u'en'
+THEME = 'pneumatic'
 
-# Feed generation is usually not desired when developing
+ARTICLE_URL = '{date:%Y}/{date:%m}/{slug}/'
+ARTICLE_SAVE_AS = ARTICLE_URL + 'index.html'
+
+PAGE_URL = '{slug}/'
+PAGE_SAVE_AS = PAGE_URL + 'index.html'
+
+ARCHIVES_SAVE_AS = 'archive/index.html'
+YEAR_ARCHIVE_SAVE_AS = '{date:%Y}/index.html'
+MONTH_ARCHIVE_SAVE_AS = '{date:%Y}/{date:%m}/index.html'
+
+# Disable authors, categories, tags, and category pages
+DIRECT_TEMPLATES = ['index', 'archives']
+CATEGORY_SAVE_AS = ''
+
+# Disable Atom feed generation
+FEED_ATOM = 'atom.xml'
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
-AUTHOR_FEED_ATOM = None
-AUTHOR_FEED_RSS = None
 
-# Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+TYPOGRIFY = True
+MD_EXTENSIONS = ['admonition', 'codehilite(linenums=True)', 'extra']
 
-# Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+CACHE_CONTENT = False
+DELETE_OUTPUT_DIRECTORY = True
+OUTPUT_PATH = 'develop'
+PATH = 'content'
 
-DEFAULT_PAGINATION = 10
+templates = ['404.html']
+TEMPLATE_PAGES = {page: page for page in templates}
 
-# Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
+STATIC_PATHS = ['images', 'uploads', 'extra']
+IGNORE_FILES = ['.DS_Store', 'pneumatic.scss', 'pygments.css']
+
+extras = ['CNAME', 'favicon.ico', 'keybase.txt', 'robots.txt']
+EXTRA_PATH_METADATA = {'extra/%s' % file: {'path': file} for file in extras}
+
+PLUGIN_PATHS = ['plugins']
+PLUGINS = ['assets', 'neighbors', 'render_math']
+ASSET_SOURCE_PATHS = ['static']
+ASSET_CONFIG = [
+    ('cache', False),
+    ('manifest', False),
+    ('url_expire', False),
+    ('versions', False),
+]
